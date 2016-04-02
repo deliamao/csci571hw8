b1209605cd484ca0031e68019335c06cf152b6b2
dlinmao <deliamao@users.noreply.github.com>
2016-04-02 12:31:55 -0700

    Update hw8Stock.php

--- hw8Stock.php
+++ hw8Stock.php
@@ -1,12 +1,14 @@
-<?php
+<?php 
 header("Access-Control-Allow-Origin: *");
+        
+      if(isset($_GET["input"])) {
+        $jsonUrl = "http://dev.markitondemand.com/MODApis/Api/v2/Lookup/json?input=".$_GET["input"];
+    	$mjson = file_get_contents($jsonUrl);
+    	echo $mjson;
+         //echo $_GET["userInput"]
+        }
+  
+  
 
-	if(isset($_GET["input"])) {
-        //use api to get the Json file
-        $quoteURL = "http://dev.markitondemand.com/MODApis/Api/v2/Quote/json?symbol=".$_GET["input"];
-        //$quoteURL = "http://dev.markitondemand.com/MODApis/Api/v2/Quote/json?symbol=AAPL";
-        $mjson = file_get_contents($quoteURL);
-        echo $mjson;
-	//}
-	
 ?>
+
